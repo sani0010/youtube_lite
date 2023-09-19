cb398f5d119c9303157ab219d8eea100568d066f
sani0010 <sanitamrakar001@gmail.com>
2023-09-19 19:11:15 +0545

    application

---
+++ src/context/contextApi.js
@@ -0,0 +1,38 @@
+import React, {createContext, useState, useEffect} from "react";
+
+import { fetchDataFromApi } from '../utils/api';
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setsearchResults] = useState(false);
+    const [selectCategories, setselectCategories] = useState("New");
+    const [mobileMenu, setmobileMenu] = useState(false);
+    useEffect(()=>{fetchSelectedCategoriesData(selectCategories)
+    },[selectCategories]);
+
+    const fetchSelectedCategoriesData = (query) =>{
+        setLoading(true)
+        fetchDataFromApi(`search/?q=${query}`).then((res) =>{
+            console.log(res)
+            // setsearchResults(res)
+            setLoading(false)
+        })
+    }
+
+    return(
+        <Context.Provider value={{
+            loading,
+            setLoading,
+            searchResults,
+            setsearchResults,
+            selectCategories,
+            setselectCategories,
+            mobileMenu,
+            setmobileMenu,
+        }}>
+            {props.children}
+        </Context.Provider>
+    )
+}
